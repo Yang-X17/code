895a7a7c8ec371a26b9b5b17d900fee318e20f50
Yang-X17 <xueyangm@outlook.com>
2025-04-23 17:49:30 +0800

    Update MK.R

--- MK.R
+++ MK.R
@@ -1,11 +1,11 @@
-setwd("D:\\1.Topic\\4.other\\model")#设定工作路径
+setwd("D:\\1.Topic\\4.other\\model")#Set default storage working path
 
 library(xlsx)
-library(trend)#MK检验的包
+library(trend)#package for MK test
 data<-read.xlsx("D:/1.Topic/4.other/model/3.dropdata.xlsx" ,3)
 mk_p<- numeric(length = 1071) 
 mk_z<- numeric(length = 1071)
-#循环 定义一个数据范围，定义一个数据操作，在这个范围中遍历这个操作
+
 for (i in 1:1071){
 a<- as.numeric(data[,c(i)])
 b<-mk.test(a,continuity = TRUE)
@@ -13,4 +13,4 @@ mk_p[i] <- as.numeric(b[2])
 mk_z[i] <- as.numeric(b[3])
 }
 df<-data.frame(mk_p,mk_z)
-write.csv(df,"D:/1.Topic/4.other/model/df.csv")#保存输出结果，存在工作路径中
+write.csv(df,"D:/1.Topic/4.other/model/df.csv")
